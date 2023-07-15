d5d84e11716d951b22b282185c822da829729b56
Kasper Gałkowski <k@galkowski.xyz>
2023-07-15 22:23:32 +0200

    A random hackball

--- server.lisp
+++ server.lisp
@@ -126,6 +126,10 @@
 (defmethod stop ((handler handler))
   (log:debug "stopping ~a" handler))
 
+;; Connection factory
+
+(defclass http-connection-factory ()
+  ())
 
 ;; Connector
 
@@ -134,6 +138,104 @@
 
 (defgeneric open (connector))
 (defgeneric close (connector))
+
+;; AbstractConnector
+
+(defclass abstract-connector (life-cycle)
+  ((%default-protocol :initform nil)
+   (%acceptors :initform nil)
+   (%lock :initform (bt:make-lock))
+   (%accepting :initform t))
+  (:default-initargs
+   :acceptors 1))
+
+;; TODO(kasper) implement
+(defun get-cpu-count ()
+  4)
+
+(defclass acceptor ()
+  ((%connector :initform nil)
+   (%id :initform nil)
+   (%name :initform nil)))
+
+(defmacro while (expr &body body)
+  `(loop while ,expr do ,@body))
+
+(defun accept (id)
+  ;; TODO (kasper) implement)
+
+(defmethod run ((acceptor acceptor))
+  (let* ((thread (bt:current-thread))
+         (name (bt:thread-name thread)))
+    ;; Can't set thread name in Common Lisp :-(
+    ;; (setf name (format nil "~a-acceptor~d@~x-~a"
+    ;;                    name %id (sxhash acceptor) acceptor
+
+    ;; TODO(kasper) handle priority
+
+    (with-slots (%connector %id)
+        acceptor
+      (with-slots (%lock %acceptors %accepting)
+          %connector
+        (bt:with-lock-held (%lock)
+          (setf (aref %acceptors %id) thread))
+        (unwind-protect
+             (catch 'break
+               (while (running-p %connector)
+                 (catch 'continue
+                   (bt:with-lock-held (%lock)
+                     (when (and (not %accepting)
+                                (running-p %connector))
+                       (throw 'continue nil))))
+                 (handler-case
+                     (accept %id)
+                   (t (x)
+                     (unless (handle-accept-failure x)
+                       (throw 'break nil))))))
+          
+          ;; TODO(kasper) clean up thread
+          nil)))))
+              
+            
+                    
+                
+    
+                       
+  
+(defmethod initialize-instance :after
+    ((connector abstract-connector) &key acceptors)
+  (let ((cores (get-cpu-count)))
+    (with-slots (%acceptors)
+        connector
+      (when (minusp acceptors)
+        (setf acceptors (max 1 (min 4 (/ cores 8)))))
+      (when (> acceptors cores)
+        (warn "Acceptors should be <= available processors: ~a" connector))
+      (setf %acceptors (make-array acceptors)))))
+
+(defmethod start ((connector abstract-connector))
+  (with-slots (%default-protocol %default-connection-factory)
+      connector
+    (unless %default-protocol
+      (error "No default protocol for ~a" connector))
+    (setf %default-connection-factory
+          (get-connection-factory %default-protocol))
+    (unless %default-connection-factory
+      (error "No protocol factory for default protocol ~a" %default-protocol))
+    ;; TODO(kasper) TLS
+    ;; (when-let ((ssl (get-connection-factory :ssl-connection-factory)))
+    
+  
+
+;; The {@link Executor} service is used to run all active tasks needed by this
+;; connector such as accepting connections * or handle HTTP requests. The
+;; default is to use the {@link Server#getThreadPool()} as an executor.
+;;
+;; I.E. use %thread-pool of the server
+
+(defclass server-connector (connector)
+  ((%port :initform 0))
+  (:documentation ""))
 
 ;; Server
                  
@@ -193,3 +295,32 @@
 
 
   
+(defclass abstract-foo ()
+  (thing))
+
+(defmethod initialize-instance :around ((foo abstract-foo) &key)
+  (print "around abstract foo")
+  (call-next-method))
+
+(defmethod initialize-instance :before ((foo abstract-foo) &key)
+  (print "before abstract foo"))
+
+(defmethod initialize-instance :after ((foo abstract-foo) &key)
+  (print "after abstract foo"))
+
+(defclass foo (abstract-foo)
+  ())
+
+(defmethod initialize-instance :around ((foo foo) &key)
+  (print "around normal foo")
+  (call-next-method))
+
+(defmethod initialize-instance :before ((foo foo) &key)
+  (print "before normal foo"))
+
+(defmethod initialize-instance :after ((foo foo) &key)
+  (print "after normal foo"))
+
+(progn
+  (make-instance 'foo)
+  (terpri))
